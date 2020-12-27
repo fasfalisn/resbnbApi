86ad49af7b5158c2f0b64c68b4e04af0c09c3f3b
Nikiforos Fasfalis <fasfalis.n@gmail.com>
2020-12-27 15:23:34 +0200

    added models

--- src/app.js
+++ src/app.js
@@ -51,7 +51,7 @@ app.all('*', (req, res, next) => {
     next(err);
 });
 
-// app.use(errorMiddleware);
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => 
     console.log(`Server running on port 3000`));
