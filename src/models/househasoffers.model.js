df6ecbc5f9b7b4ce65e4d8268c74bdbfddac3b56
Nikiforos Fasfalis <fasfalis.n@gmail.com>
2020-12-31 18:35:19 +0200

    fixed query

--- src/models/househasoffers.model.js
+++ src/models/househasoffers.model.js
@@ -1,26 +1,22 @@
-const query = require('../db/db-connection');
-const { multipleColumnSet } = require('../utils/common.utils');
+const query = require("../db/db-connection");
+const { multipleColumnSet } = require("../utils/common.utils");
 
 class HouseModel {
-    tableName = 'house_has_offers';
-    wh = 'valid';
+  tableName = "house_has_offers";
+  wh = "valid";
 
-    find = async (params = {}) => {
-        let sql = `SELECT Type,Name FROM ${this.tableName} JOIN house ON house.houseid = house_has_offers.houseid WHERE status = ?`;
+  find = async (params = {}) => {
+    let sql = `SELECT HouseID,Type,Name FROM ${this.tableName} JOIN house ON house.houseid = house_has_offers.houseid WHERE status = ?`;
 
-        if (!Object.keys(params).length) {
-            return await query(sql, [this.wh]);
-        }
-
-        const { columnSet, values } = multipleColumnSet(params)
-        sql += ` WHERE ${columnSet}`;
-
-        return await query(sql, [...values]);
+    if (!Object.keys(params).length) {
+      return await query(sql, [this.wh]);
     }
 
+    const { columnSet, values } = multipleColumnSet(params);
+    sql += ` WHERE ${columnSet}`;
 
+    return await query(sql, [...values]);
+  };
 }
 
-
-
-module.exports = new HouseModel;
+module.exports = new HouseModel();
