bf6e0d016be473a30606c1afee0774f4fec533ba
Nikiforos Fasfalis <fasfalis.n@gmail.com>
2021-01-11 19:08:28 +0200

    added host post

--- src/db/db-connection.js
+++ src/db/db-connection.js
@@ -9,10 +9,10 @@ class DBConnection {
             user: "be46e85583b1dd",
             password: "c4306dc5",
             database: "heroku_f7f2b5ee759b774",
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASS,
-            database: process.env.DB_DATABASE
+            // host: process.env.DB_HOST,
+            // user: process.env.DB_USER,
+            // password: process.env.DB_PASS,
+            // database: process.env.DB_DATABASE
         });
 
         this.checkConnection();
